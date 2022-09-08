e02792ee90e8a5fde07a4faa7ac0c89989762c62
Rafael-Batista-Dev <dev.rafaelbatista@gmail.com>
2022-09-08 09:54:09 -0300

    Config

--- checkout/next.config.js
+++ checkout/next.config.js
@@ -1,7 +1,45 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "checkout",
+          filename: "static/chunks/remoteEntry.js",
+          remotes: {
+            checkout:
+              "checkout@http://localhost:3001/_next/static/chunks/remoteEntry.js",
+          },
+          exposes: {
+            // pages
+            "./pages/checkout/index": "./pages/checkout/index.js",
+            "./pages/checkout/products/[...slug]":
+              "./pages/checkout/products/[...slug].js",
+            "./pages/checkout/test-webpack-png":
+              "./pages/checkout/test-webpack-png.js",
+            "./pages/checkout/test-webpack-svg":
+              "./pages/checkout/test-webpack-svg.js",
+            "./pages/checkout/exposed-pages":
+              "./pages/checkout/exposed-pages.js",
+            // components
+            "./useCustomRemoteHook": "./components/useCustomRemoteHook.js",
+            "./WebpackSvg": "./components/WebpackSvg.js",
+            "./WebpackPng": "./components/WebpackPng.js",
+            // utilities
+            "./pages/_menu": "./pages/_menu.js",
+            "./pages-map": "./pages-map.js",
+          },
+          extraOptions: {
+            enableImageLoaderFix: true,
+            enableUrlLoaderFix: true,
+          },
+        })
+      );
+    }
+    return config;
+  },
+};
--- home/next.config.js
+++ home/next.config.js
@@ -1,7 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "home",
+          filename: "static/chunks/remoteEntry.js",
+          remotes: {
+            home: "home@http://localhost:3000/_next/static/chunks/remoteEntry.js",
+            shop: "shop@http://localhost:3001/_next/static/chunks/remoteEntry.js",
+            checkout:
+              "checkout@http://localhost:3002/_next/static/chunks/remoteEntry.js",
+          },
+          exposes: {
+            "./NavBar": "./components/NavBar.js",
+            "./Footer": "./components/Footer.js",
+          },
+          shared: {},
+          extraOptions: {
+            exposePages: true,
+            enableImageLoaderFix: true,
+            enableUrlLoaderFix: true,
+          },
+        })
+      );
+    }
+
+    return config;
+  },
+};
